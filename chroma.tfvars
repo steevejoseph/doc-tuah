8e1188fd9e26b57947002362852706ab335ac3f8
Steeve Joseph <steevejoseph@knights.ucf.edu>
2024-12-25 16:54:54 -0500

    Add terraform

--- chroma.tfvars
+++
@@ -1,6 +0,0 @@
-# TODO(steevejoseph): When Azure account finishes creating, follow the rest of this guide:
-# https://docs.trychroma.com/production/cloud-providers/azure
-
-resource_group_name = "your-azure-resource-group-name"
-location            = "your-location"           
-machine_type        = "Standard_B1s"
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from api import v1
+import chromadb
 
 app = FastAPI()
 
@@ -9,3 +10,8 @@ app.include_router(v1.router, prefix="/api/v1")
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+chroma_client = chromadb.HttpClient(host="20.169.211.240", port=8000)
+
+print(f"chroma hearbeat: {chroma_client.heartbeat()}")
