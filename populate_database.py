bec08f4c95934a91c46415ffab4270ea9b095c7c
Steeve Joseph <steevejoseph@knights.ucf.edu>
2024-12-25 09:20:48 -0500

    Update populate_database to get rid of deprecation warnings

--- populate_database.py
+++ populate_database.py
@@ -1,12 +1,12 @@
 import argparse
 import os
 import shutil
-from langchain.document_loaders.pdf import PyPDFDirectoryLoader
-from langchain.document_loaders.md import TextLoader
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_community.document_loaders.text import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.schema.document import Document
 from get_embedding_function import get_embedding_function
-from langchain.vectorstores.chroma import Chroma
+from langchain_chroma import Chroma
 
 
 CHROMA_PATH = "chroma"
